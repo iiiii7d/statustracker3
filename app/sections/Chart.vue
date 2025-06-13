b99f7919bf3f073e340ec5e05c69b89103361e86
7d <61975820+iiiii7d@users.noreply.github.com>
2025-06-14 00:55:26 +1000

    change nameToUUID url to fix random 403s

--- app/sections/Chart.vue
+++ app/sections/Chart.vue
@@ -84,7 +84,7 @@ export async function updatePlayer() {
         ? null
         : (
             await useAsyncData(
-              `player:${shownPlayer.value}:${from.value}:${to.value}`,
+              `playe vr:${shownPlayer.value}:${from.value}:${to.value}`,
               () =>
                 $fetch(`/player/${shownPlayer.value}`, {
                   query: {
